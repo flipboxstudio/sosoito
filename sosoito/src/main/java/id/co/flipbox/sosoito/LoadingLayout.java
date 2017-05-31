04fa2e5c3d29d51ae511186f427fa2780d374bee
suhafer <suryo@flipbox.co.id>
2017-05-31 18:24:19 +0700

    [Feature] Add & implement custom loading view.

--- sosoito/src/main/java/id/co/flipbox/sosoito/LoadingLayout.java
+++ sosoito/src/main/java/id/co/flipbox/sosoito/LoadingLayout.java
@@ -1,7 +1,9 @@
 package id.co.flipbox.sosoito;
 
 import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.annotation.TargetApi;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -14,12 +16,17 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class LoadingLayout extends RelativeLayout {
+    public static final long DEFAULT_ANIMATION_DURATION = 2500L;
     private View           loadingView;
     private TextView       loadingMessage;
     private View           emptyView;
     private ImageView      emptyIcon;
     private TextView       emptyMessage;
     private ProgressDialog progressDialog;
+    private View           customLoadingView;
+    private ImageView      customLoadingIcon;
+    private TextView       customLoadingMessage;
+    private AnimatorSet    animatorSet;
 
     public LoadingLayout(Context context) {
         super(context);
@@ -45,6 +52,7 @@ public class LoadingLayout extends RelativeLayout {
     private void init() {
         inflate(getContext(), R.layout.progressbar, this);
         inflate(getContext(), R.layout.empty_view, this);
+        inflate(getContext(), R.layout.customprogressbar, this);
         reset();
     }
 
@@ -63,6 +71,22 @@ public class LoadingLayout extends RelativeLayout {
         progressDialog.setIndeterminate(true);
         progressDialog.setCancelable(false);
         progressDialog.setCanceledOnTouchOutside(false);
+
+        customLoadingView = findViewById(R.id.customprogressbar);
+        customLoadingIcon = (ImageView) findViewById(R.id.cpb_icon);
+        customLoadingMessage = (TextView) findViewById(R.id.cpb_message);
+        emptyIcon.setImageResource(R.drawable.cpb_default_icon);
+        emptyMessage.setText(R.string.text_loading_message);
+        defaultAnimatorSetup();
+    }
+
+    private void defaultAnimatorSetup()
+    {
+        AnimatorSet cLIAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.flip);
+        cLIAnimatorSet.setTarget(customLoadingIcon);
+        animatorSet = new AnimatorSet();
+        animatorSet.play(cLIAnimatorSet);
+        animatorSet.setDuration(DEFAULT_ANIMATION_DURATION);
     }
 
     public void setProgressDialog(ProgressDialog progressDialog)
@@ -106,6 +130,7 @@ public class LoadingLayout extends RelativeLayout {
             this.getChildAt(i).setVisibility(show ? View.INVISIBLE : View.VISIBLE);
         }
         emptyView.setVisibility(View.GONE);
+        customLoadingView.setVisibility(View.GONE);
         loadingView.setVisibility(show ? View.VISIBLE : View.GONE);
     }
 
@@ -115,6 +140,7 @@ public class LoadingLayout extends RelativeLayout {
             this.getChildAt(i).setVisibility(show ? View.INVISIBLE : View.VISIBLE);
         }
         emptyView.setVisibility(View.GONE);
+        customLoadingView.setVisibility(View.GONE);
         loadingView.setVisibility(show ? View.VISIBLE : View.GONE);
         loadingMessage.setText(message);
         loadingMessage.setVisibility(show ? View.VISIBLE : View.GONE);
@@ -164,5 +190,85 @@ public class LoadingLayout extends RelativeLayout {
         emptyIcon.setImageResource(iconResId);
         emptyMessage.setText(message);
         showEmptyView(show);
+    }
+
+    public void showCustomLoading(final boolean show) {
+        for (int i=0; i < this.getChildCount(); i++)
+        {
+            this.getChildAt(i).setVisibility(show ? INVISIBLE : VISIBLE);
+        }
+        emptyView.setVisibility(GONE);
+        loadingView.setVisibility(GONE);
+        customLoadingView.setVisibility(show ? VISIBLE : GONE);
+        if (show)
+            animatorSet.start();
+        else
+            animatorSet.end();
+    }
+
+    public void showCustomLoading(final boolean show, final String message) {
+        for (int i=0; i < this.getChildCount(); i++)
+        {
+            this.getChildAt(i).setVisibility(show ? INVISIBLE : VISIBLE);
+        }
+        emptyView.setVisibility(GONE);
+        loadingView.setVisibility(GONE);
+        customLoadingView.setVisibility(show ? VISIBLE : GONE);
+        customLoadingMessage.setText(message);
+        customLoadingMessage.setVisibility(show ? VISIBLE : GONE);
+        if (show)
+            animatorSet.start();
+        else
+            animatorSet.end();
+    }
+
+    public void showCustomLoading(final boolean show, int iconResId) {
+        for (int i=0; i < this.getChildCount(); i++)
+        {
+            this.getChildAt(i).setVisibility(show ? INVISIBLE : VISIBLE);
+        }
+        emptyView.setVisibility(GONE);
+        loadingView.setVisibility(GONE);
+        customLoadingView.setVisibility(show ? VISIBLE : GONE);
+        customLoadingIcon.setImageResource(iconResId);
+        if (show)
+            animatorSet.start();
+        else
+            animatorSet.end();
+    }
+
+    public void showCustomLoading(final boolean show, int iconResId, final String message) {
+        for (int i=0; i < this.getChildCount(); i++)
+        {
+            this.getChildAt(i).setVisibility(show ? INVISIBLE : VISIBLE);
+        }
+        emptyView.setVisibility(GONE);
+        loadingView.setVisibility(GONE);
+        customLoadingView.setVisibility(show ? VISIBLE : GONE);
+        customLoadingIcon.setImageResource(iconResId);
+        customLoadingMessage.setText(message);
+        customLoadingMessage.setVisibility(show ? VISIBLE : GONE);
+        if (show)
+            animatorSet.start();
+        else
+            animatorSet.end();
+    }
+
+    public void setCLVAnimator(int animatorResId)
+    {
+        AnimatorSet cLIAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), animatorResId);
+        cLIAnimatorSet.setTarget(customLoadingIcon);
+        animatorSet = new AnimatorSet();
+        animatorSet.play(cLIAnimatorSet);
+        animatorSet.setDuration(DEFAULT_ANIMATION_DURATION);
+    }
+
+    public void setCLVAnimator(int animatorResId, long duration)
+    {
+        AnimatorSet cLIAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), animatorResId);
+        cLIAnimatorSet.setTarget(customLoadingIcon);
+        animatorSet = new AnimatorSet();
+        animatorSet.play(cLIAnimatorSet);
+        animatorSet.setDuration(duration);
     }
 }
